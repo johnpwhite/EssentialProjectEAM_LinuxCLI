cb5e1fe9a7e1ed11b99fd744d84ea56c45fd8b29
John White <42159626+johnpwhite@users.noreply.github.com>
2020-06-06 21:38:47 +1000

    Add files via upload

--- run_protege_server_fix.sh
+++ run_protege_server_fix.sh
@@ -1,42 +1,6 @@
 #!/bin/sh
-
-# ------------------- Where is Java? ------------------- 
-
-# Change to the script' working directory, should be the Protege root directory 
-#cd $(dirname $0)
-echo "Starting Protege Server in "
-pwd
-DARWIN="false"
-
-if [  -x /usr/bin/uname ]
-then
-  if [ "x`/usr/bin/uname`" = "xDarwin" ] 
-  then
-    DARWIN="true"
-  fi
-fi
-
-if [ ${DARWIN} = "true" ]
-then
-  JAVA_PATH=/usr/bin
-else 
-  # Attempt to use the bundled VM if none specified
-  if [ "$JAVA_HOME" = "" ]; then
-	JAVA_HOME=.
-  fi
-
-  JAVA_PATH=$JAVA_HOME/jre/bin
-fi
-
-# Check if the Java VM can be found 
-if [ ! -e $JAVA_PATH/java ]; then
-	echo Java VM could not be found. Please check your JAVA_HOME environment variable.
-	exit 1
-fi
-# ------------------- Where is Java? ------------------- 
-
-
-CLASSPATH=protege.jar:looks.jar:driver.jar:driver0.jar:driver1.jar:driver2.jar
+JAVA_PATH=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/
+CLASSPATH=protege.jar:looks.jar:driver.jar
 MAINCLASS=edu.stanford.smi.protege.server.Server
 
 
@@ -45,9 +9,9 @@ MAX_MEMORY=-Xmx2048M
 HEADLESS=-Djava.awt.headless=true
 CODEBASE_URL=file:/root/Protege_3.5/protege.jar
 CODEBASE=-Djava.rmi.server.codebase=$CODEBASE_URL
-HOSTNAME_PARAM=-Djava.rmi.server.hostname=localhost
+HOSTNAME_PARAM=-Djava.rmi.server.hostname=ubuntutemplate.localdomain
 TX="-Dtransaction.level=READ_COMMITTED"
-LOG4J_OPT="-Dlog4j.configuration=file:log4j.xml"
+LOG4J_OPT="-Dlog4j.configuration=file:/root/Protege_3.5/logs/log4j.xml"
 
 OPTIONS="$MAX_MEMORY $HEADLESS $CODEBASE $HOSTNAME_PARAM ${TX} ${LOG4J_OPT}"
 
